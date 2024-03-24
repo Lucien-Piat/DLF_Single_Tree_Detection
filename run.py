fb69bd62197121b716f36a4a21876216b924f50d
nomomon <funisinfinity26@gmail.com>
2024-03-24 13:13:29 +0100

    Add image loading functionality to run.py

--- run.py
+++ run.py
@@ -1,11 +1,19 @@
 from ultralytics import YOLO
+import os
 
 if __name__ == "__main__":
+    # Get image
+    image_dir = 'data/gold/valid'
+    image_files = os.listdir(image_dir)
+    image_files = [file for file in image_files if file.endswith('.jpg')]
+    image_files.sort()
+    image_path = os.path.join(image_dir, image_files[0])
+
     # Load the model
     model = YOLO('runs/segment/train/weights/best.pt')
 
     # Run the model on a single image
-    results = model('data/gold/valid/0__  1_102_    1_    1.jpg')
+    results = model(image_path)
 
     # Display the results
     results[0].show()
